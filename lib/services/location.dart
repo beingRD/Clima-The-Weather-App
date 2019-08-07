00b16f320e4f585005c1446e8d10ad0de8de043e
Rishabh <rishabhdev.work@gmail.com>
2019-08-07 17:44:47 +0200

    pos/code

---
+++ lib/services/location.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class Location extends StatelessWidget {
+  //Location({@required this.latitude, @required this.longitude});
+
+  double latitude;
+  double longitude;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
